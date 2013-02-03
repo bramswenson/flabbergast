208c2a6c08bf81f34a4b7513ace46c767e7ec0b0
Bram Swenson <bram@craniumisajar.com>
2013-02-03 11:16:43 -0800

    [GRIDGEN] speced

---
+++ spec/flabbergast/grid_generator_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'GridGenerator' do
+  let(:size) { 4 }
+  subject    { Flabbergast::GridGenerator.new(size) }
+
+  it 'picks letters from the standard alphabet' do
+    Flabbergast::GridGenerator::ALPHABET.should be == ('A'..'Z').to_a
+  end
+
+  it 'generates a random grid of the specified size' do
+    subject.grid.size.should be == size
+  end
+
+end
