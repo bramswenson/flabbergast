144c0ca302bff3ec7f9dad67a7b002444a97efb9
Bram Swenson <bram@craniumisajar.com>
2013-02-03 15:33:08 -0800

    [FINDER] not solver

--- lib/flabbergast/grid_word_finder.rb
+++ lib/flabbergast/grid_word_finder.rb
@@ -1,10 +1,10 @@
 require 'set'
 require 'celluloid'
-require 'flabbergast/string_solver'
+require 'flabbergast/string_word_finder'
 
 module Flabbergast
   # Used to find all the words in a word grid puzzle
-  class Solver
+  class GridWordFinder
 
     # @param [Flabbergast::Dictionary] dictionary a dictionary of valid words
     # @param [Array[Array]] grid an array of arrays of letters
@@ -15,10 +15,10 @@ module Flabbergast
 
     # Search the grid for words from the dictionary
     # @return [Set] a set of words found on the grid
-    def solve!
+    def find_words!
       words = Set.new
       futures = %w( rows columns diagonals ).map do |direction|
-        send "solve_#{direction}"
+        send "find_#{direction}"
       end
       words += words_from_futures(futures)
       words.sort
@@ -33,36 +33,36 @@ module Flabbergast
       end
     end
 
-    def solve_and_collect_words(string)
+    def find_and_collect_words(string)
       futures = [ string, string.reverse ].map do |string|
-        Flabbergast::StringSolver.new(string, @dictionary).solve
+        Flabbergast::StringWordFinder.new(string, @dictionary).find_words!
       end
       words_from_futures(futures)
     end
 
-    # NOTE: long term these solve_ methods might be better as classes
-    def solve_rows
+    # NOTE: long term these find_ methods might be better as classes
+    def find_rows
       Celluloid::Future.new do
         # search each row and its substrings forward and back
         @grid.reduce([]) do |words, row|
-          words += solve_and_collect_words(row.join)
+          words += find_and_collect_words(row.join)
           words
         end
       end
     end
 
-    def solve_columns
+    def find_columns
       Celluloid::Future.new do
         # search each column and its substrings forward and back
         (0..@grid.size-1).to_a.reduce([]) do |words, n|
           row_string = @grid.map{|row| row[n]}
-          words += solve_and_collect_words(row_string)
+          words += find_and_collect_words(row_string)
           words
         end
       end
     end
 
-    def solve_diagonals
+    def find_diagonals
       Celluloid::Future.new do
         # search each diagonal and its substrings forward and back
         # only works corner to corner at the moment, needs quite a bit
@@ -78,8 +78,8 @@ module Flabbergast
           word << @grid[x][@grid.size-x]
           word
         end
-        words += solve_and_collect_words(diag1.join)
-        words += solve_and_collect_words(diag2.join)
+        words += find_and_collect_words(diag1.join)
+        words += find_and_collect_words(diag2.join)
         words
       end
     end
--- lib/flabbergast/string_word_finder.rb
+++ lib/flabbergast/string_word_finder.rb
@@ -2,7 +2,7 @@ require 'celluloid'
 
 module Flabbergast
   # Used to find words in a string of letters
-  class StringSolver
+  class StringWordFinder
 
     # @param [String] string a string of letters to be searched
     # @param [Flabbergast::Dictionary] dictionary a dictionary of valid words
@@ -14,7 +14,7 @@ module Flabbergast
 
     # Recursively search the string for words from the dictionary
     # @return [Celluloid::Future] a future that finds words in the string
-    def solve
+    def find_words!
       Celluloid::Future.new do
         (0..(@size-1)).to_a.reduce([]) do |words, position|
           current_string = @string[position..-1]
@@ -22,7 +22,7 @@ module Flabbergast
             if @dictionary.words.has_key?(current_string)
               words << [ current_string, @dictionary.words.get(current_string) ]
             end
-            words += self.class.new(current_string[0..-2], @dictionary).solve.value
+            words += self.class.new(current_string[0..-2], @dictionary).find_words!.value
           end
           words
         end
