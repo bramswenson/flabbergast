e566cf991d845b933963d194ced259678b4af636
Bram Swenson <bram@craniumisajar.com>
2013-02-03 11:04:02 -0800

    [GRID] spec and refactor for SRP

--- lib/flabbergast/grid.rb
+++ lib/flabbergast/grid.rb
@@ -1,40 +1,25 @@
 require 'flabbergast/grid_loader'
 require 'flabbergast/grid_generator'
 require 'flabbergast/dictionary'
-require 'terminal-table'
 
 module Flabbergast
   class Grid
-    attr_reader :words, :size
+    attr_reader :raw_grid
 
     def initialize(options)
-      @options = options
-      @grid = load_grid_from_file(options.file) if options.file
-      @grid ||= generate_grid(@options.size)
-      @size = @grid.size
+      if options.file
+        @raw_grid = load_grid_from_file(options.file)
+      elsif options.size
+        @raw_grid ||= generate_grid(options.size)
+      end
+      raise ArgumentError, 'size or file required' if @raw_grid.nil?
     end
 
-    def solve!
-      @words = solver.solve!
-    end
-
-    def to_s
-      Terminal::Table.new(title: title, rows: @grid).to_s unless @grid.empty?
+    def size
+      @raw_grid.size
     end
 
     private
-
-    def title
-      "#{@grid.size}x#{@grid.size} Wordgrid"
-    end
-
-    def solver
-      @solver ||= Flabbergast::Solver.new(dictionary, @grid)
-    end
-
-    def dictionary
-      @dictionary ||= Flabbergast::Dictionary.new(@options.dictionary_file)
-    end
 
     def load_grid_from_file(file)
       Flabbergast::GridLoader.new(file).grid
---
+++ spec/flabbergast/grid_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require 'ostruct'
+
+describe 'Grid' do
+  subject { Flabbergast::Grid.new(options) }
+
+  describe '#initialize' do
+
+    context 'given a size option' do
+      let(:size)    { 8 }
+      let(:options) { OpenStruct.new(size: size) }
+
+      it 'creates a grid with the specified size' do
+        subject.raw_grid.size.should be == size
+      end
+
+      it 'sets the size attribute' do
+        subject.size.should be == size
+      end
+    end
+
+    context 'given a file option' do
+      let(:file)    { File.expand_path('../../fixtures/test_grid.txt', __FILE__) }
+      let(:options) { OpenStruct.new(file: file) }
+
+      it 'loads the grid from the file' do
+        subject.raw_grid.size.should be == 4
+      end
+
+      it 'sets the size attribute' do
+        subject.size.should be == 4
+      end
+
+      it 'uses the grid from the file' do
+        subject.raw_grid.should be == [
+          %w( C A B S ),
+          %w( B A T S ),
+          %w( F A T S ),
+          %w( G A P S ),
+        ]
+      end
+    end
+
+    context 'given a no size or file option' do
+      let(:options) { OpenStruct.new }
+      it 'throws an error' do
+        expect{ Flabbergast::Grid.new(options) }.to raise_error('size or file required')
+      end
+    end
+  end
+end
