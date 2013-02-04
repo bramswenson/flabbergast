7bb150a3922eed16d8e66e32373e8d86a8f2bea0
Bram Swenson <bram@craniumisajar.com>
2013-02-03 18:05:07 -0800

    [GRIDFINDER] now with inheritance flavor

--- lib/flabbergast/grid_word_finder.rb
+++ lib/flabbergast/grid_word_finder.rb
@@ -3,9 +3,9 @@ require 'celluloid'
 require 'flabbergast/string_word_finder'
 
 module Flabbergast
-  # Used to find all the words in a word grid puzzle
-  class GridWordFinder
 
+  # Base class for all finders wrapping futures helpers and initalization
+  class BaseGridWordFinder
     # @param [Flabbergast::Dictionary] dictionary a dictionary of valid words
     # @param [Array[Array]] grid an array of arrays of letters
     def initialize(dictionary, grid)
@@ -13,18 +13,7 @@ module Flabbergast
       @grid       = grid
     end
 
-    # Search the grid for words from the dictionary
-    # @return [Set] a set of words found on the grid
-    def find_words!
-      words = Set.new
-      futures = %w( rows columns diagonals ).map do |direction|
-        send "find_#{direction}"
-      end
-      words += words_from_futures(futures)
-      words.sort
-    end
-
-    private
+    protected
 
     def words_from_futures(futures)
       futures.reduce([]) do |words, future|
@@ -39,21 +28,43 @@ module Flabbergast
       end
       words_from_futures(futures)
     end
+  end
 
-    # NOTE: long term these find_ methods might be better as classes
-    def find_rows
+  # Used to find all the words in a word grid puzzle
+  class GridWordFinder < BaseGridWordFinder
+
+    # Search the grid for words from the dictionary
+    # @return [Set] a set of words found on the grid
+    def find_words!
+      words = Set.new
+      #futures = %w( rows columns diagonals ).map do |direction|
+      futures = [
+        Flabbergast::GridRowWordFinder,
+        Flabbergast::GridColumnWordFinder,
+        Flabbergast::GridDiagonalWordFinder,
+      ].map do |finder|
+        finder.new(@dictionary, @grid).find_words!
+      end
+      words += words_from_futures(futures)
+      words.sort
+    end
+  end
+  # Used to find all the words in a word grid row
+  class GridRowWordFinder < BaseGridWordFinder
+    def find_words!
       Celluloid::Future.new do
-        # search each row and its substrings forward and back
         @grid.reduce([]) do |words, row|
           words += find_and_collect_words(row.join)
           words
         end
       end
     end
+  end
 
-    def find_columns
+  # Used to find all the words in a word grid column
+  class GridColumnWordFinder < BaseGridWordFinder
+    def find_words!
       Celluloid::Future.new do
-        # search each column and its substrings forward and back
         (0..@grid.size-1).to_a.reduce([]) do |words, n|
           row_string = @grid.map{|row| row[n]}
           words += find_and_collect_words(row_string)
@@ -61,8 +72,11 @@ module Flabbergast
         end
       end
     end
+  end
 
-    def find_diagonals
+  # Used to find all the words in a word grid diagonal
+  class GridDiagonalWordFinder < BaseGridWordFinder
+    def find_words!
       Celluloid::Future.new do
         # search each diagonal and its substrings forward and back
         # only works corner to corner at the moment, needs quite a bit
